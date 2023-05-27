bb454ddc361bdca57c8524a2299a6ca4cf131b2e
tabina <111294387+tabinasns@users.noreply.github.com>
2023-05-27 23:54:42 +0700

    Add files via upload

---
+++ storage/framework/views/a29681477f5e14d35a01ca7d771c643f.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\kuliah\SEMESTER 4\PEMROGRAMAN FRAMEWORK\LARAVEL\praktikum5,6,7\resources\views/profile.blade.php ENDPATH**/ ?>
